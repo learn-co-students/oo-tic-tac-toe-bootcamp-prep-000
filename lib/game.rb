91efeb199b9086589d8c29fb00087eefd0bf0cce
M. Simon Borg <msimonborg@gmail.com>
2016-11-11 17:09:09 -0500

    made Game.match a global variable

--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,8 @@ require_relative '../lib/one_player.rb'
 # The Game class is the actual game object that controls the flow
 # of one or more game(s) of Tic Tac Toe
 class Game
+  attr_accessor :match
+
   def initialize
     puts 'Welcome to Tic Tac Toe!'
   end
@@ -12,12 +14,12 @@ class Game
     puts ''
     puts 'One or two players? (press 1 or 2 and hit <return>)'
     @players = gets.strip
-    @match = if @players == '1'
+    $match = if @players == '1'
                OnePlayer.new
              elsif @players == '2'
                TwoPlayer.new
              end
-    return unless @match.nil?
+    return unless $match.nil?
     gets_players
   end
 
@@ -25,9 +27,9 @@ class Game
     puts "Let's play!"
     sleep(1)
     puts ''
-    @match.board.display
+    $match.board.display
     puts ''
-    @match.turn until @match.over?(@match.board)
+    $match.turn until $match.over?($match.board)
     won_or_draw
     return unless play_again?
     gets_players
@@ -35,8 +37,8 @@ class Game
   end
 
   def won_or_draw
-    puts "Congratulations, #{@match.winner.name}!" if @match.won?(@match.board)
-    puts 'Cats Game!' if @match.draw?(@match.board)
+    puts "Congratulations, #{$match.winner.name}!" if $match.won?($match.board)
+    puts 'Cats Game!' if $match.draw?($match.board)
   end
 
   def play_again?
