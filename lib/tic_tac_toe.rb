fa0025553497ab82fc20dfe374200805be55ff80
John Shelby <johnshelby@yahoo.com>
2016-11-22 22:17:08 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,4 @@
-require 'pry'
 class TicTacToe
-
-  def initialize(board=[" "," "," "," "," "," "," "," "," "])
-    @board=board
-  end
   WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -15,118 +10,106 @@ class TicTacToe
     [2,4,6],
   ]
 
-def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
-
-def move(index, char = "X")
-  index = (index).to_i - 1
-  @board[index]=char
-end
-
-def position_taken?(index)
-   (@board[index]=="X" || @board[index]=="O")
-end
-
-def valid_move?(index)
-  index = (index.to_i - 1)
-     !position_taken?(index) && index.between?(0,8)
-end
-
-def turn
-  puts "Please enter 1-9:"
-  position = gets.strip
-  @index = (position.to_i - 1)
-    #binding.pry
-  if valid_move?(position)
-    move(position,current_player)
-    display_board
-  else
-    turn
+  def initialize(board=[" "," "," "," "," "," "," "," "," "])
+    @board=board
   end
-end
 
-def turn_count
-  occupied = 0
-  @board.each do |space|
-    if !(space == "" || space == " ")
-      occupied+=1
-    end
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  return occupied
-end
 
-def current_player
-  if turn_count%2==0
-    return "X"
-  else
-    return "O"
+  def move(index, char = "X")
+    index = (index).to_i - 1
+    @board[index]=char
   end
-end
 
-
-
-def won?
-WIN_COMBINATIONS.each do |wincombo|
-  if wincombo.all? do |square|
-     @board[square] == "X"
-   end
-     return wincombo
-   elsif wincombo.all? do |square|
-     @board[square] == "O"
-   end
-     return wincombo
-   end
- end
-  return false
-end
-
-def full?
-  @board.all? do |position|
-    position != " "
+  def position_taken?(index)
+     (@board[index]=="X" || @board[index]=="O")
   end
-end
 
-def draw?
-  !won? && full?
-end
-
-def over?
-  if draw? || won?
-    return true
+  def valid_move?(index)
+    index = (index.to_i - 1)
+    !position_taken?(index) && index.between?(0,8)
   end
-end
 
-def winner
-  if won? != false
-    return @board[won?[0]]
-  end
-end
-
-
-
-
-
-def play
-  while !over?
+  def turn
+    puts "Please enter 1-9:"
+    position = gets.strip
+    if valid_move?(position)
+      move(position,current_player)
+      display_board
+    else
       turn
     end
-  if draw?
-    puts "Cats Game!"
-  else
-    puts "Congratulations #{winner}!"
   end
-end
 
+  def turn_count
+    occupied = 0
+    @board.each do |space|
+      if !(space == "" || space == " ")
+        occupied+=1
+      end
+    end
+    return occupied
+  end
 
+  def current_player
+    if turn_count%2==0
+      return "X"
+    else
+      return "O"
+    end
+  end
 
+  def won?
+    WIN_COMBINATIONS.each do |wincombo|
+      if wincombo.all? do |square|
+         @board[square] == "X"
+       end
+         return wincombo
+       elsif wincombo.all? do |square|
+         @board[square] == "O"
+       end
+         return wincombo
+       end
+     end
+      return false
+  end
 
+  def full?
+    @board.all? do |position|
+      position != " "
+    end
+  end
 
+  def draw?
+    !won? && full?
+  end
 
+  def over?
+    if draw? || won?
+      return true
+    end
+  end
 
+  def winner
+    if won? != false
+      return @board[won?[0]]
+    end
+  end
 
+  def play
+    while !over?
+        turn
+      end
+    if draw?
+      puts "Cats Game!"
+    else
+      puts "Congratulations #{winner}!"
+    end
+  end
 end
