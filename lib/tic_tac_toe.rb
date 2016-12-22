ed1f3135fb513a8dd7bfdd6e603238a6dce5695a
Devin Rieger <dev0089@gmail.com>
2016-12-22 01:37:59 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,81 +1,3 @@
-=begin
-class TicTacToe
-
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ]
-  
-  def initialize(board = nil)
-    @board = board || Array.new(9, " ")
-  end
-
-  #def initialize(index)
-  #  @index = input.to_i - 1
-  #end
-  
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-
-  def move(input, current_player)
-    @board[input.to_i-1] = current_player
-  end
-
-  def position_taken?(input)
-    !(@board[input].nil? || @board[input] == " ")
-  end
-
-  def valid_move?(input)
-    input.between?(1,9) && !input_taken?(input-1)
-  end
-
-  def turn
-     display_board
-     puts "Please enter 1-9:"
-     input = gets.strip
-     if !valid_move?(input)
-       turn
-     end
-     move(input, current_player)
-     display_board
-   end
-
-   def draw?
-     !won? && @board.all?{|current_player| current_player == "X" || current_player == "O"}
-   end
-
-
-  def full?
-    @board.all?{|current_player| current_player == "X" || current_player == "O"}
-  end
-
-  def over?
-    won? || draw?
-  end
-  
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
-  end
- 
-  def turn_count
-    @board.count{|current_player| current_player == "X" || current_player == "O"}
-  end
-  
-end
-=end
-
-
 class TicTacToe
 
   WIN_COMBINATIONS = [
@@ -102,16 +24,20 @@ class TicTacToe
   end
 
   def play
+    
     puts "Welcome to Tic Tac Toe!"
     display_board
-    while !over?
+    
+    until over?
       turn
     end
+    
     if won?
       puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cats Game!"
     end
+    
   end
 
   def turn
@@ -134,11 +60,11 @@ class TicTacToe
   end
 
   def full?
-    @board.all?{|token| token == "X" || token == "O"}
+    @board.all?{|index| !(index.nil? || index == " ")}
   end
 
   def over?
-    won? || draw?
+    draw? || !!won? || full?
   end
 
   def turn_count
@@ -150,20 +76,20 @@ class TicTacToe
   end
 
   def won?
-    WIN_COMBINATIONS.detect do |combo|
-      position(combo[0]) == position(combo[1]) &&
-      position(combo[1]) == position(combo[2]) &&
-      position_taken?(combo[0])
-    end
+    WIN_COMBINATIONS.detect{|combo|
+      combo.all?{|index| position_taken?(index)} and
+      (combo.all?{|index| @board[index] == 'X'} or
+      combo.all?{|index| @board[index] == 'O'})
+    }
   end
 
   def draw?
-    !won? && @board.all?{|token| token == "X" || token == "O"}
+    !(!!won?) && full?
   end
 
   def winner
-    if winning_combo = won?
-      @winner = position(winning_combo.first)
+    if over?
+      @board[won?[0]]
     end
   end
 
