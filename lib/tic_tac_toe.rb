854a58c007e2b1746372b16a3bc82c0b79ac921c
Matthew Ahern <matt.r.ahern@gmail.com>
2017-01-29 22:31:15 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,96 @@
+class TicTacToe
+
+   WIN_COMBINATIONS = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]]
+
+   def initialize(board = nil)
+     @board = board || Array.new(9, " ")
+   end
+
+   def current_player
+     turn_count % 2 == 0 ? "X" : "O"
+   end
+
+   def turn_count
+     @board.count{|token| token == "X" || token == "O"}
+   end
+
+   def display_board
+     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+     puts "-----------"
+     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+     puts "-----------"
+     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+   end
+
+   def input_to_index(number)
+     number.to_i - 1
+   end
+
+   def move(index, player_character = "X")
+     @board[index] = player_character
+   end
+
+   def position_taken?(index)
+     if @board[index] == " " || @board[index] == "" || @board[index] == nil
+       false
+     elsif @board[index] == "X" || @board[index] == "O"
+       true
+     end
+   end
+
+   def valid_move?(index)
+     if index.between?(0, 8) && !position_taken?(index)
+         true
+     end
+   end
+
+   def turn
+     puts "Please enter 1-9:"
+     input = gets.strip
+     index = input_to_index(input)
+     if valid_move?(index)
+       move(index, current_player)
+       display_board
+     else
+       turn
+     end
+   end
+
+   def won?
+     WIN_COMBINATIONS.detect do |win_combination|
+       @board[win_combination[0]] == @board[win_combination[1]] &&
+       @board[win_combination[1]] == @board[win_combination[2]] &&
+       position_taken?(win_combination[0])
+     end
+   end
+
+   def full?
+     @board.all?{|player| player == "X" || player == "O"}
+   end
+
+   def draw?
+     !won? && full?
+   end
+
+   def over?
+     won? || full? || draw?
+   end
+
+   def winner
+     if winning_combination = won?
+       @board[winning_combination.first]
+     end
+   end
+
+   def play
+     while !over?
+       turn
+     end
+
+     if won?
+       puts "Congratulations #{winner}!"
+     elsif draw?
+       puts "Cat's Game!"
+     end
+   end
+ end
