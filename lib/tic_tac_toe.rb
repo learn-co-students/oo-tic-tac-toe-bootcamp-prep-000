18bbc257a3e18b0a92af6d8a11ca338b7c2d9c6c
Nancy Noyes <nnoyes@comcast.net>
2017-04-21 14:04:42 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,16 @@ class TicTacToe
     @board = Array.new(9, " ")
     display_board
   end
+
+  def board
+    @board
+  end
+
+  def board=(board)
+    @board = board
+  end
+
+
   WIN_COMBINATIONS = [
     [0,1,2], # Top row
     [3,4,5],  # Middle row
@@ -13,148 +23,98 @@ class TicTacToe
     [0,4,8],  # First diagonal
     [2,4,6]  # Second diagonal
   ]
+
   def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 
   def input_to_index(index)
-    @index = index
-    @index = @index.to_i
-    @index = @index - 1
+    index = index.to_i
+    index = index - 1
   end
 
   def move(index, player = "X")
-    @board[index] = player
+    board[index] = player
   end
 
-  def position_taken?
-    if @board[@index] == "X" || @board[@index] == "O"
+  def position_taken?(index)
+    if board[index] == "X" || board[index] == "O"
       true
     else
-      if @board[@index] == "" || @board[@index] == " " || @board[@index] == nil
+      if board[index] == "" || board[index] == " " || board[index] == nil
         false
       end
     end
   end
 
-  def valid_move?
-    if position_taken?
+  def valid_move?(index)
+    if position_taken?(index)
       false
     else
-      if @index.between?(0, 8)
+      if index.between?(0, 8)
         true
       end
     end
   end
 
   def turn
+    player = current_player
     puts "Please enter 1-9:"
     input = gets.strip
-    @index = input_to_index(input)
-    if valid_move?
-      move(@index, @player)
-    else
-      turn
-    end
+    index = input_to_index(input)
+    valid_move?(index) ? move(index, player) : turn
     display_board
   end
 
   def turn_count
-    @counter = 0
-    @board.each do |turn_number|
+    counter = 0
+    board.each do |turn_number|
       if turn_number == "X" || turn_number =="O"
-        @counter += 1
+        counter += 1
       end
     end
-    @counter
+    return counter
   end
 
   def current_player
-    turn_count
-    @counter.even? ? @player = "X" : @player = "O"
+    turn_count.even? ? "X" : "O"
   end
 
-  def won?
-    position = Array.new(3)
-    if @board.all?{|i| i == "" || i == " "}
-      false
-    else WIN_COMBINATIONS.each do |win_combination|
-        i=0
-        while i <3
-          position[i] = @board[win_combination[i]]
-          i +=1
-        end
 
-        if position.all?{|value| value == "X"}
-          return win_combination # return the win_combination indexes that won.
-        else
-          if position.all?{|value| value == "O"}
-            return win_combination
-          end
-        end
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      if win_combination.all?{|y| board[y] == "X"} || win_combination.all?{|y| board[y] == "O"}
+        return win_combination
       end
     end
-    false
+    return false
   end
 
   def full?
-    if @board.all?{|i| i == "X" || i == "O"}
-      true
-    else
-      false
-    end
+    board.all?{|i| i == "X" || i == "O"} ? true : false
   end
 
   def draw?
-    if !won?&& full?
-      true
-    else
-      false
-    end
+    !won? && full? ? true : false
   end
 
   def over?
-    if draw? || full? || won? != false
-      true
-    else
-      false
-    end
+    draw? || won? != false ? true : false
   end
 
   def winner
-    if won?== false
-      nil
-    else
-      if @board[won?([0]] == "X"
-        "X"
-      else
-        "O"
-      end
-    end
-
+    won? == false ? nil : (board[won?[0]] == "X" ? "X" : "O")
   end
 
-  def play(
+  def play
     while !over?
       turn
     end
-    if won? != false
-      puts "Congratulations #{winner(board)}!"
-    else
-      if draw?
-        puts "Cats Game!"
-      end
-    end
+    draw? ? (puts "Cat's Game!") : (puts "Congratulations #{winner}!")
   end
-
-
-
-
-
-
 
 end
