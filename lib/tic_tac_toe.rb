0036678340c0f4a9ddb9d70d930cbef471fbd03c
Matt Milton <matt.milton@gmail.com>
2017-08-04 17:46:38 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,76 @@
+class TicTacToe
+  def initialize
+    @board=board=Array.new(9," ")
+  end
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n-----------\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n-----------\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i-1
+  end
+  def move(index,token="X")
+    @board[index] = token
+  end
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
+    #!(board[index].nil? || board[index] == " ")
+  end
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  def turn_count
+    @board.count{|token| token!=" "}
+  end
+  def current_player
+    turn_count % 2 ==0 ? "X": "O"
+  end
+  def turn
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      token = current_player
+      move(index, token)
+    else
+      turn
+    end
+    display_board
+  end
+
+  def won?
+      WIN_COMBINATIONS.any? do |combo|
+        if (@board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] && position_taken?(combo[0]))
+          return combo
+        end
+      end
+    end
+  def full?
+      @board.all?{|token| token == "X" || token == "O"}
+    end
+  def draw?
+      !won? && full?
+    end
+  def over?
+      won? || draw?
+    end
+  def winner
+      if combo = won?
+        @board[combo[0]]
+      end
+    end
+  def play
+      turn until over?
+      puts winner ? "Congratulations #{winner}!": "Cat's Game!"
+  end
+end
