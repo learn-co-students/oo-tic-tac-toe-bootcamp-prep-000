77e230b06962f2cc1c000d0425e6a5205b943b47
Carolyn Yuan <carolynlyuan@gmail.com>
2017-08-10 19:54:28 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -125,4 +125,15 @@ class TicTacToe
     end
   end
 
+  def play
+    while over? == false
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
 end
