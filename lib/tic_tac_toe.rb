05e504b472f255249415a948565811b8bec17581
beardshadow <beardshadow@gmail.com>
2017-08-21 23:25:49 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,4 +89,15 @@ class TicTacToe
     end
   end
 
+  def play
+    while !over?
+      turn;
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
 end
