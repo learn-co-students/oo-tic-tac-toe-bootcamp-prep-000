2ce8a805d21401bc3fd0c8729769d2cca693e9a7
PeterBadal <peter.badal@gmail.com>
2017-09-10 23:01:52 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,6 +51,7 @@ class TicTacToe
     else
       turn
     end
+  end
 =begin
     def turn_count
       @board.count{|token| token == "X" || token == "O"}
@@ -65,12 +66,71 @@ def turn_count
     end
     end
     counter
-end
-
+    end
 
 def current_player
    turn_count % 2 == 0 ? "X" : "O"
  end
 
+ def won?
+   WIN_COMBINATIONS.each do |win_combo| # win_combo is same as WIN_COMBINATIONS[0][1][2]
+     if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]]== "X") || (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
+      return win_combo
+     end
+     end
+     return false
+ end
+
+ def full?
+   @board.all? do |element|
+     element == "X" || element == "O"
+ end
+ end
+
+ def draw?
+   !won? && full?
+ end
+
+ def over?
+   won? || draw?
+ end
+
+ def winner
+   if won?
+      #@board[won?(@board[0]]
+      WIN_COMBINATIONS.each do |win_combo| # win_combo is same as WIN_COMBINATIONS[0][1][2]
+        if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]]== "X") || (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
+         return @board[win_combo[0]]
+        end
+      end
+    end
+ end
+
+ def draw?
+   !won? && full?
+ end
+
+ def over?
+   won? || draw?
+ end
+
+ def play
+   until over?
+     turn
+   end
+     #move(current_player(board), input_to_index())
+   if won?
+     puts("Congratulations #{winner}!")
+   elsif draw?
+   puts("Cat's Game!")
+ end
+ end
+
+ def initialize(board = nil)
+   @board = board || Array.new(9, " ")
+ end
 
 end
+# we have game which will be the board array
+# won?(game) will be either false, or an array (for example: [0,1,2])
+# we can't grab a value of an index for false
