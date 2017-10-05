fc80d061575f7da46f79fe334ce6d811afa88451
Anthony Grimes <agrimes@charter.net>
2017-10-05 02:39:27 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,19 +27,39 @@ class TicTacToe
     end
 
     def move(index, player)
-
+      board[position] = player
     end
 
     def position_taken?(index)
-
+      if board[position] == "X" || board[position] == "O"
+         true
+      else
+         false
+      end
     end
 
     def valid_move?(index)
-
+      if position.between?(0,8) && !position_taken?(board, position)
+         true
+      else
+         false
+      end
     end
 
     def turn
+      puts "Please enter 1-9:"
+      move = gets.strip
 
+      int = input_to_index(move)
+
+      if valid_move?(board, int)
+          move(board, int, current_player(board))
+          display_board(board)
+      else
+          puts "Please enter 1-9:"
+          move = gets.strip
+          int = input_to_index(move)
+      end
     end
 
     def turn_count
@@ -51,26 +71,81 @@ class TicTacToe
     end
 
     def won?
+      if board.all? { |spot| spot.strip == ""} == false
+         false
+      end
 
+      if (board[WIN_COMBINATIONS[0][0]] == "X" && board[WIN_COMBINATIONS[0][1]] == "X" && board[WIN_COMBINATIONS[0][2]] == "X") || (board[WIN_COMBINATIONS[0][0]] == "O" && board[WIN_COMBINATIONS[0][1]] == "O" && board[WIN_COMBINATIONS[0][2]] == "O")
+         return WIN_COMBINATIONS[0]
+      elsif (board[WIN_COMBINATIONS[1][0]] == "X" && board[WIN_COMBINATIONS[1][1]] == "X" && board[WIN_COMBINATIONS[1][2]] == "X") || (board[WIN_COMBINATIONS[1][0]] == "O" && board[WIN_COMBINATIONS[1][1]] == "O" && board[WIN_COMBINATIONS[1][2]] == "O")
+         return WIN_COMBINATIONS[1]
+      elsif (board[WIN_COMBINATIONS[2][0]] == "X" && board[WIN_COMBINATIONS[2][1]] == "X" && board[WIN_COMBINATIONS[2][2]] == "X") || (board[WIN_COMBINATIONS[2][0]] == "O" && board[WIN_COMBINATIONS[2][1]] == "O" && board[WIN_COMBINATIONS[2][2]] == "O")
+         return WIN_COMBINATIONS[2]
+      elsif (board[WIN_COMBINATIONS[3][0]] == "X" && board[WIN_COMBINATIONS[3][1]] == "X" && board[WIN_COMBINATIONS[3][2]] == "X") || (board[WIN_COMBINATIONS[3][0]] == "O" && board[WIN_COMBINATIONS[3][1]] == "O" && board[WIN_COMBINATIONS[3][2]] == "O")
+         return WIN_COMBINATIONS[3]
+      elsif (board[WIN_COMBINATIONS[4][0]] == "X" && board[WIN_COMBINATIONS[4][1]] == "X" && board[WIN_COMBINATIONS[4][2]] == "X") || (board[WIN_COMBINATIONS[4][0]] == "O" && board[WIN_COMBINATIONS[4][1]] == "O" && board[WIN_COMBINATIONS[4][2]] == "O")
+         return WIN_COMBINATIONS[4]
+      elsif (board[WIN_COMBINATIONS[5][0]] == "X" && board[WIN_COMBINATIONS[5][1]] == "X" && board[WIN_COMBINATIONS[5][2]] == "X") || (board[WIN_COMBINATIONS[5][0]] == "O" && board[WIN_COMBINATIONS[5][1]] == "O" && board[WIN_COMBINATIONS[5][2]] == "O")
+         return WIN_COMBINATIONS[5]
+      elsif (board[WIN_COMBINATIONS[6][0]] == "X" && board[WIN_COMBINATIONS[6][1]] == "X" && board[WIN_COMBINATIONS[6][2]] == "X") || (board[WIN_COMBINATIONS[6][0]] == "O" && board[WIN_COMBINATIONS[6][1]] == "O" && board[WIN_COMBINATIONS[6][2]] == "O")
+         return WIN_COMBINATIONS[6]
+      elsif (board[WIN_COMBINATIONS[7][0]] == "X" && board[WIN_COMBINATIONS[7][1]] == "X" && board[WIN_COMBINATIONS[7][2]] == "X") || (board[WIN_COMBINATIONS[7][0]] == "O" && board[WIN_COMBINATIONS[7][1]] == "O" && board[WIN_COMBINATIONS[7][2]] == "O")
+         return WIN_COMBINATIONS[7]
+      else
+         false
+      end
     end
 
     def full?
-
+      if board.all? { |spot| spot.strip != ""} == true
+         true
+      else
+         false
+      end
     end
 
     def draw?
-
+      if full?(board) && !won?(board)
+         true
+      end
     end
 
     def over?
-
+      if draw?(board)
+         true
+      elsif full?(board) && won?(board)
+         true
+      elsif !full?(board) && won?(board)
+         true
+      else
+         false
+      end
     end
 
     def winner
+      if !won?(board) && full?(board)
+         return false
+      end
 
+      if won?(board)
+         win = won?(board)
+         if board[win[0]] == "X"
+            "X"
+         elsif board[win[0]] == "O"
+            "O"
+         end
+      end
     end
 
     def play
+      until over?(board) do
+         turn(board)
+      end
 
+      if won?(board)
+         puts "Congratulations #{winner(board)}!"
+      elsif draw?(board)
+         puts "Cat's Game!"
+      end
     end
 end
