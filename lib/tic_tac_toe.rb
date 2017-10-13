4be3d15eb870b11cec514669d0554fa7e4bd44dc
gitluky <jluk87@gmail.com>
2017-10-13 15:54:43 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,102 @@
+# Helper Method
+class TicTacToe
+
+def initialize
+  @board = Array.new(9, " ")
+end
+
+
+def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
+end
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+def won?
+  WIN_COMBINATIONS.detect do |set|
+    @board[set[0]] == @board[set[1]] &&
+    @board[set[1]] == @board[set[2]] &&
+    position_taken?(set[0])
+  end
+end
+
+def full?
+  @board.all? {|value| value == "X" || value == "O"}
+end
+
+def draw?
+  full? && !won?
+end
+
+def over?
+  won? || draw?
+end
+
+def winner
+  if winning_combo = won?
+    @board[winning_combo.first]
+  end
+end
+
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+def turn_count
+  count = 0
+  @board.each do |location|
+    if location != " "
+      count+=1
+    end
+  end
+  return count
+end
+
+def current_player
+  turn_count % 2 == 0 ? "X": "O"
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(index, current_player)
+  @board[index] = current_player
+end
+
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
+end
+
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(index)
+    move(index, current_player)
+    display_board
+  else
+    turn
+  end
+end
+
+# Define your play method below
+def play
+  display_board
+  until over?
+    turn
+  end
+
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
+
+end
