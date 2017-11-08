4b5698ef74c017762e69cfb6e22f0ac828c509ab
Annette Michaels <micha232@umn.edu>
2017-11-08 21:32:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,7 +44,7 @@ class TicTacToe
 
 
   def valid_move?(index)
-    index.between?(0, 8) && !position_taken?(index) ? true : false
+    (index.between?(0, 8) && !position_taken?(index)) ? true : false
   end
 
 
