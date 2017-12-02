592f45a364ca959ebb0e6e91e81d9eea1df14cc2
Karen Zhao <karen.zhao18@gmail.com>
2017-12-02 05:06:40 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,129 +4,115 @@ class TicTacToe
     @board = Array.new(9, " ") || board
   end
 
-WIN_COMBINATIONS = [
-	  [0,1,2],
-	  [3,4,5],
-	  [6,7,8],
-	  [0,3,6],
-	  [1,4,7],
-	  [2,5,8],
-	  [0,4,8],
-	  [6,4,2]
-	]
+  WIN_COMBINATIONS = [
+  	  [0,1,2],
+  	  [3,4,5],
+  	  [6,7,8],
+  	  [0,3,6],
+  	  [1,4,7],
+  	  [2,5,8],
+  	  [0,4,8],
+  	  [6,4,2]
+  	]
 
-	def play
-	  while !over?
-	    turn
-
-	  end
-	  if won?
-	    puts "Congratulations #{winner}!"
-	  elsif draw?
-	    puts "Cat's Game!"
-	  end
-	end
-
-
-	def display_board
-	  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-	  puts "-----------"
-	  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-	  puts "-----------"
-	  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-	end
+  def play
+  	  while !over?
+  	    turn
+  	  end
+  	  if won?
+  	    puts "Congratulations #{winner}!"
+  	  elsif draw?
+  	    puts "Cat's Game!"
+  	  end
+  end
 
 
-	def valid_move?(index)
-	  index.between?(0,8) && !position_taken?(index)
-	end
+  def display_board
+  	 puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  	 puts "-----------"
+  	 puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  	 puts "-----------"
+  	 puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
 
-	def won?
-	  WIN_COMBINATIONS.find do |combo|
-	    @board[combo[0]] == @board[combo[1]] && @board[combo[0]] == @board[combo[2]] && position_taken?(combo[1])
-	  end
-	end
+  def won?
+  	 WIN_COMBINATIONS.find do |combo|
+  	   @board[combo[0]] == @board[combo[1]] && @board[combo[0]] == @board[combo[2]] && position_taken?(combo[1])
+  	 end
+  end
 
 
-	def full?
-	  @board.none? do |i|
+  def full?
+    @board.none? do |i|
       i == " " || i.nil?
     end
-	end
+  end
+
+  def draw?
+  	 won? == nil && full? == true
+  end
 
 
-	def draw?
-	  won? == nil && full? == true
-	end
+  def over?
+  	 won? != nil || draw? == true
+  end
 
 
-	def over?
-	  won? != nil || draw? == true
-	end
+  def input_to_index(input)
+    return input.to_i - 1
+  end
 
-
-	def input_to_index(inp)
-	  s = inp.to_i
-    s - 1
-	end
-
-
-	def turn
-	  puts "Please enter 1-9: "
-	  inp = gets.strip
-	  index = input_to_index(inp)
-	  if valid_move?(index)
-	    move(index, current_player)
-	  else !valid_move?(index)
-      until valid_move?(index)
-        puts "Sorry, invalid move. Please enter 1-9: "
-        display_board
-        inp = gets.strip
-        index = input_to_index(inp)
-        valid_move?(index)
-  	    move(index, current_player)
-      end
-	  end
-	end
-
-
-	def position_taken?(index)
-	  !(@board[index].nil? || @board[index] == " ")
-	end
-
-
-	def current_player
-	  if turn_count % 2 == 0
-      current_player = "X"
+  def valid_move?(index)
+    if index.between?(0,8) && !position_taken?(index)
+      return true
     else
-      current_player = "O"
+      return nil
     end
-	end
+  end
 
-
-	def turn_count
-    count = 0
-	  @board.each do |move|
-      if move == "X" || move == "O"
-        count += 1
+  def turn
+  	puts "Please enter 1-9: "
+  	input = gets.strip
+  	index = input_to_index(input)
+  	if valid_move?(index) == true
+      move(index, current_player)
+    else
+      until valid_move?(index)
+        puts "Please enter 1-9: "
+      	input = gets.strip
+      	index = input_to_index(input)
       end
     end
-	end
+  end
 
 
-	def move(index, player)
-	  @board[index] = player
-    # turn_count
-    # display_board
-	end
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
 
 
-	def winner
-	  if won? != nil
-	    winner = @board[won?[0]]
-	  end
-	end
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+
+
+  def move(index, current_player)
+  	 @board[index] = current_player
+      # turn_count
+      # display_board
+  end
+
+
+  def winner
+  	 if won? != nil
+  	   winner = @board[won?[0]]
+  	 end
+  end
 
 end
 
