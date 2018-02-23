62731cfe3dbfbecba9f4dc8faacc4d4a4e569679
Peter Coughlin <coughlin1966@gmail.com>
2018-02-23 20:49:24 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,14 +5,26 @@ def initialize(board = nil)
 end
 
 WIN_COMBINATIONS = [
-  [0, 1, 2]
-  [3, 4, 5]
-  [6, 7, 8]
-  [0, 3, 6]
-  [1, 4, 7]
-  [2, 5, 8]
-  [0, 4, 8]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
   [2, 4, 6]
 ]
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+def input_to_index(input)
+@input = input.to_i - 1
+end
 
 end
