fd4e0ad86154c8086ec87da708fcca6e2f9f489a
Peter Coughlin <coughlin1966@gmail.com>
2018-02-23 20:21:01 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,5 +4,15 @@ def initialize(board = nil)
   @board = board || Array.new(9, " ")
 end
 
+WIN_COMBINATIONS = [
+  [0, 1, 2]
+  [3, 4, 5]
+  [6, 7, 8]
+  [0, 3, 6]
+  [1, 4, 7]
+  [2, 5, 8]
+  [0, 4, 8]
+  [2, 4, 6]
+]
 
 end
