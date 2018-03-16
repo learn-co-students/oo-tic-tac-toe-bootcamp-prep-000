b04587004f59442f5deb10f0b00587ced0e350d8
Vanessa Toro <vanetoro84@gmail.com>
2018-03-16 18:35:00 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,17 +1,40 @@
 class TicTacToe
   WIN_COMBINATIONS = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,5,9],
-    [3,5,7],
-    [2,5,8],
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
     [1,4,7],
-    [3,6,9]
+    [0,3,6],
+    [2,5,8]
     ]
   def initialize
     @board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',]
   
 end
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  puts "-----------"
+end
+
+def input_to_index(input)
+  return input = input.to_i - 1
+end
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  
+  def position_taken?
+    if @board[input_to_index(input)] =  " "
+      return false
+    end+
+  end
   
 end 
