0506e0434190c2040cb6e6711c75caf6649dbec2
Sam Parsons <samparsons269@gmail.com>
2018-04-25 00:40:41 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,6 +2,7 @@ class TicTacToe
   def initialize
     @board = Array.new(9, " ")
   end
+
   WIN_COMBINATIONS = [[0,1,2],
     [3,4,5],
     [6,7,8],
@@ -10,6 +11,7 @@ class TicTacToe
     [2,5,8],
     [0,4,8],
     [2,4,6]]
+
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -17,18 +19,23 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+
   def input_to_index(user_input)
     user_input.to_i - 1
   end
+
   def move(index, current_player = "X")
     @board[index] = current_player
   end
+
   def position_taken?(location)
     @board[location] != " " && @board[location] != ""
   end
+
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
+
   def turn_count
     count = 0
     @board.each do |element|
@@ -38,6 +45,7 @@ class TicTacToe
     end
     return count
   end
+
   def current_player
     if turn_count % 2 == 0
       return "X"
@@ -45,9 +53,62 @@ class TicTacToe
       return "O"
     end
   end
+
   def won?
     WIN_COMBINATIONS.find do |win_combo|
       @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[0]] == @board[win_combo[2]] && position_taken?(win_combo[1])
     end
+  end
+
+  def full?
+    @board.all? { |item| item != " "}
+  end
+
+  def draw?
+    if !won? && full?
+      return true
+    elsif !won? && !full?
+      return false
+    elsif won?
+      return false
+    end
+  end
+
+  def over?
+    if won? || full? || draw?
+      return true
+    else
+      return false
+    end
+  end
+
+  def winner
+    index = []
+    index = won?
+    if index == nil
+      return nil
+    else
+      if @board[index[0]] == "X"
+        return "X"
+      else
+        return "O"
+      end
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+
+  def play
+
   end
 end
