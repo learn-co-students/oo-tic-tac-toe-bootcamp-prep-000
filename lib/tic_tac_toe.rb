d3ff2705e9981e55769e496323bcfc73b1ca8554
Sam Parsons <samparsons269@gmail.com>
2018-04-25 00:23:01 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,53 @@
+class TicTacToe
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  WIN_COMBINATIONS = [[0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]]
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  def move(index, current_player = "X")
+    @board[index] = current_player
+  end
+  def position_taken?(location)
+    @board[location] != " " && @board[location] != ""
+  end
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  def turn_count
+    count = 0
+    @board.each do |element|
+      if element != " "
+        count += 1
+      end
+    end
+    return count
+  end
+  def current_player
+    if turn_count % 2 == 0
+      return "X"
+    else
+      return "O"
+    end
+  end
+  def won?
+    WIN_COMBINATIONS.find do |win_combo|
+      @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[0]] == @board[win_combo[2]] && position_taken?(win_combo[1])
+    end
+  end
+end
