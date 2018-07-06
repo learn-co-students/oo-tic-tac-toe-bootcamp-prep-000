c84da3d9a0c624adc9318a0185997f455008cc0a
gmat191@gmail.com <gmat191@gmail.com>
2018-07-06 00:04:30 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,11 +39,9 @@ class TicTacToe
     end 
   end
 
-def valid_move?(board, index)
-     if index.between?(0,8) && !position_taken?(board, index)
-       return true
-     end
-   end
+def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
 
 
   #def turn
@@ -57,16 +55,17 @@ def valid_move?(board, index)
     #display_board
   #end
   
-  def turn(board) 
-  puts "Please enter 1-9:"    
-  input = gets.strip   
-  index = input_to_index(input)     
-  if valid_move?(board, index)
-     move(board, index, current_player(board))
-     else 
-     turn(board)
-   end           
-   display_board(board)
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
 end
 
 
