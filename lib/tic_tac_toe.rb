9c47885722149575fd1284d3d0b812c9ac4f22da
Jing Cao <jingcao.yale@gmail.com>
2018-07-24 16:20:16 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,4 +23,73 @@ class TicTacToe
   end
 
   def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
   end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn_count
+    counter = 0
+    @board.each do |position|
+      if position == "X" || position == "O"
+        counter += 1
+      end
+    end
+    return counter
+  end
+
+  def current_player
+    turn_count % 2 == 0? "X" : "O"
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, player_token = current_player)
+      display_board
+    else
+      puts "Invalid"
+      turn
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |combo|
+      return combo if (@board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" ||
+      @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O" )
+    end
+    false if @board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def full?
+    true if @board.none?{|i| i == " "}
+  end
+
+  def draw?
+    true if full? && !won?
+  end
+
+  def over?
+    true if draw? || won? || full?
+  end
+
+  def winner
+    if won?
+      win_array = won?
+      return @board[win_array[0]]
+    end
+  end
+
+  def play
+    while !over?
+      turn
+    end
+    puts "Congratulations #{winner}!"
+    puts "Cat\'s Game!"
+  end
+
+end
