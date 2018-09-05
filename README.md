af2c71b48f2f957a8545b70425e2627946eeb16d
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-09-05 21:41:05 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# OO Tic Tac Toe
+c Ta# OO Tic Tac Toe
 
 ## Objectives
 
---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,11 @@
+class TicTacToe
+  def initialize
+@board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+end
+end
+
+  WIN_COMBINATIONS = [
+    [0,1,2], [3,4,5], [6,7,8],[0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]
+    ]
+   
