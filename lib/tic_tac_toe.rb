fe256c3f4529651146c6a7382c789ea2766c38d4
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-09-06 18:19:38 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,15 +10,23 @@ class TicTacToe
 end
 
 
-def display_board(board)
+def display_board()
 
 
 
-puts " #{board[0]} | #{board[1]} | #{board[2]} " 
+puts " #{@board[0]} | #{@board[1]} | #{@board[2]} " 
 puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "  
+puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "  
 puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} " 
+puts " #{@board[6]} | #{@board[7]} | #{@board[8]} " 
 
 
 end
+end 
+
+def input_to_index(user_input)
+  user_input.to_i - 1 
+  
+ 
+
+end 
