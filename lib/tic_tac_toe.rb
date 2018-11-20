74dec9e826c23a9a02ed55efb09fcdac6805101c
Billy Tan <btan7645@gmail.com>
2018-11-20 21:12:27 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,6 @@
 class TicTacToe
-  def initialize
-    board = [" ", " ", " ", " "," ", " ", " ", " ", " "]
-    @board = board
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
   end 
   
   WIN_COMBINATIONS = [
@@ -23,5 +22,87 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} " 
   end
   
+  def input_to_index(input)
+   input.to_i - 1
+  end
   
+  def move(index, value="X")
+     @board[index] = value
+  end
+  
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
+  end
+  
+  def valid_move?(position)
+    position_taken?(position) == false && position.between?(0, 8)
+  end
+  
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+  
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end  
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
+  
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+       position_1 == "O" && position_2 == "O" && position_3 == "O" 
+      return win_combination
+    end
+  end
+    else
+      return false
+  end  
+  
+  def full?
+    @board.all? { |token| token == 'X' || token == 'O' }
+  end
+  
+  def draw?
+    !won? && full?
+  end
+  
+  def over?
+    won? || draw?
+  end
+  
+  def winner
+    if win_combination = won?
+      @board[win_combination[0]]
+    end
+  end
+  
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
 end
