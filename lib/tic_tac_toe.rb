e334d24d0b76bbe3931bfa97394bb70bbe391d42
Dawn Ann Beattie <heartandhandstraining@users.noreply.github.com>
2018-12-18 22:29:44 -0800

    Update tic_tac_toe.rb

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,9 @@
 class TicTacToe
+    
     def initialize(board = Array.new(9, " "))
         @board = board
     end
+    
     WIN_COMBINATIONS = [
         [0,1,2],
         [3,4,5],
@@ -13,13 +15,19 @@ class TicTacToe
         [2,4,6]
     ]
     
-    def play
-        def display_board
-            puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-            puts "-----------"
-            puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-            puts "-----------"
-            puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-        end
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+    
+    def input_to_index(input)
+        input.to_i-1 #convert user input to integer and subtract 1 to represent index
+    end 
+    
+    def move(index, token)
+        @board[index] = token 
     end
 end
