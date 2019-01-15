47e98eedde1d091bbe72a16039faabb0c79da928
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-15 15:20:05 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,107 @@
+class TicTacToe
+
+  def initialize(board=[" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    @board = board
+  end
+
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts '-----------'
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts '-----------'
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1 
+end
+
+def move(index, token)
+  @board[index] = token
+end
+
+def position_taken?(index)
+  (@board[index] == "X" || @board[index] == "O")
+end
+
+def valid_move?(index)
+  (!position_taken?(index) && index.between?(0, 8))
+end
+
+def turn
+  puts 'Please enter 1-9:'
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    move(index, current_player)
+    display_board
+  else
+    turn
+  end
+end
+
+def turn_count
+  n = 0 
+  @board.each do |index|
+    if index == "X" || index=="O"
+      n+= 1
+    end
+  end
+  return n
+end
+
+def current_player
+  turn_count % 2 == 0 ? "X" : "O"
+end
+
+def won?
+  WIN_COMBINATIONS.detect do |win_combo|
+    @board[win_combo[0]] == @board[win_combo[1]] &&
+      @board[win_combo[1]] == @board[win_combo[2]] &&
+      position_taken?(win_combo[0])
+  end
+end
+
+def full?
+  !@board.any?{|index| index == " "}
+end
+
+def draw?
+  (!won? && full?)
+end
+
+def over? 
+  (won? || draw?)
+end
+
+def winner
+  if won?
+    @board[won?[0]]
+  end
+end
+
+def play
+  turn until over?
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
+end
+
+
+  
+
