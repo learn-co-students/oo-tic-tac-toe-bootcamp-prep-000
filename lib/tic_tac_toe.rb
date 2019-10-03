f99a1c9725a8a4a6e7f6516ee2321c2669cbdcbb
Elias <eliasiscoding@gmail.com>
2019-10-02 21:48:07 -0400

    partially done with Charity

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,6 @@
+require 'pry'
 class TicTacToe
-  def initialize(board = nil)
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  end
+  attr_reader :board
   
   WIN_COMBINATIONS =  [
   [0,1,2],
@@ -13,141 +12,60 @@ class TicTacToe
   [0,4,8],
   [2,4,6]
 ]
-  def display_board()
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
-  def input_to_index(input)
-    input.to_i - 1 
-  end
-  
-  def move(index,character)
-    @board[index] = character
-  end
-  
-  def position_taken?(index)
 
-    taken = nil
-    if (@board[index] ==  " " || @board[index] == "" || @board[index] == nil)
-      taken = false
-    else
-      taken = true
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    if @board[index] == " "
+      false
+    else 
+      true
     end
   end
-  
+
   def valid_move?(index)
-    if position_taken?(index) || !(index.between?(0,8))
+    if @board[index] == nil || position_taken?(index)
       false
     else
       true
     end
   end
 
-  def turn()
-    puts "Enter a number from 1-9:"
-    number = gets.strip
-    index = input_to_index(number)
-    if valid_move?(index)
-      character = current_player()
-      move(index, character)
-      display_board()
-    else
-      turn()
-    end 
-  end
-  
-  def turn_count()
+  def turn_count
     counter = 0
-    @board.each do |character|
-      if character == "X" || character == "O"
+    @board.each do |el|
+      # counter += 1 if el != " "
+      if el == "X" || el == "O"
         counter += 1
-      end 
-    
-    end 
-    return counter
-  end
-  
-  def current_player()
-    if turn_count() % 2 == 0
-      return "X"
-    else 
-      return "O"
-    end
-  end
-  
-  def won?()
-  winner = nil
-  WIN_COMBINATIONS.each do |combo|
-    if combo.all? {|idx| @board[idx] == "X"}
-      winner = combo
-    elsif combo.all? {|idx|@board[idx] == "O"}
-      winner = combo
-    else
-      false 
-    end
-  end
-  if winner != nil
-    winner
-  else
-    false
-  end
-end
-
-
-  def winner()
-    winner = nil
-    WIN_COMBINATIONS.each do |combo|
-      if combo.all? {|idx| @board[idx] == "X"}
-        winner = "X"
-      elsif combo.all? {|idx| @board[idx] == "O"}
-        winner = "O"
-      else
       end
     end
-    winner
+    counter
   end
-  
-  
-  
-  
-  def full?()
-    @board.all?{ |token| token == "X" || token == "O"}
-  end
-  
-  def draw?()
-    if full?() && !(won?())
-      true
+
+  def current_player
+    # turn_count.even?
+    if turn_count % 2 == 0
+      "X"
     else
-      false 
+      "O"
     end
   end
-  
-  
-   def over?()
-    if full?() || won?() || draw?()
-      true
-    else
-      false
-    end
-  end
-  
-  
-  
-  def play()
-    until over?()
-      turn()
-    end
-    
-    if won?()
-      puts "Congratulations #{winner()}!"
-    elsif draw?()
-      puts "Cat's Game!"
-    end
-  end
-  
-        
+
 end
