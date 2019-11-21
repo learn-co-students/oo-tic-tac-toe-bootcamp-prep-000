fb35f245dd139b2176bcececfb6f25b6e65ccdfd
Vik Vangala <vvang044@gmail.com>
2019-11-21 19:19:52 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,4 +2,33 @@ class TicTacToe
   def initialize(board=nil)
     @board=board || Array.new(9," ")
   end 
+  
+  WIN_COMBINATIONS=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [6,4,2],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+    ]
+    
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+   	puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end	  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 end 
