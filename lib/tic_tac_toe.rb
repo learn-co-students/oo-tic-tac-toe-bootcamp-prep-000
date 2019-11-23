92fdf285f384228bfbb598b97aa46a0826cfdefc
Vik Vangala <vvang044@gmail.com>
2019-11-23 02:19:58 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,7 +69,35 @@ end
 def over?
   won? || draw?
 end 
+
+def winner
+  if combo = won?
+    @board[combo[0]]
+  end 
+end
   
-  
+def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
+end  
+
+def play
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
   
  end 
